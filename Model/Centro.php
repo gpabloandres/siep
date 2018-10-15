10fd4e2d94a8aebd1fde9d261c963b0de326ba32
Pablo <gpabloandres>
2018-10-15 18:41:47 -0300

    Corrige ingreso de datos del equipo directivo

--- Model/Centro.php
+++ Model/Centro.php
@@ -177,12 +177,12 @@ class Centro extends AppModel {
                     'equipoDirectivo' => array(
                            'minLength' => array(
                     	       'rule' => array('minLength', 5),
-                        	   'allowEmpty' => false,
+                        	   'allowEmpty' => true,
                            	   'message' => 'Indicar nombres del equipo directivo (Director, Vicedirector y Secretario).'
 		   		    		),
 							'alphaBet' => array(
-				            	'rule' => '/^[ áÁéÉíÍóÓúÚa-zA-ZñÑ]{5,}$/i',
-					  			'message' => 'Indicar nombres del equipo directivo (Sólo letras y espacios)'
+				            	'rule' => '/^[ áÁéÉíÍóÓúÚa-zA-ZñÑ,.]{5,}$/i',
+					  			'message' => 'Indicar nombres del equipo directivo (Sólo letras, espacios, puntos y comas)'
 		                	)
                     ),
 				    'ambito' => array(
