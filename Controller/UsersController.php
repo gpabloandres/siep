7edcc15b4ace7fe6bd4672cbbdf72cd3ebccae06
Pablo <gpabloandres>
2019-07-03 09:34:15 -0300

    Corrige mensaje de acceso en acciones no permitidas.

--- Controller/UsersController.php
+++ Controller/UsersController.php
@@ -33,11 +33,6 @@ class UsersController extends AppController {
 			case 'admin':
 				$this->Auth->allow('logout', 'usuario');
 				break;
-
-			default:
-				$this->Session->setFlash('No tiene permisos.', 'default', array('class' => 'alert alert-warning'));
-				$this->redirect($this->referer());
-				break;
 		}
 		/* FIN */
 		/* FUNCIÓN PRIVADA "LISTS" (INICIO).
